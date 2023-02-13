50d1cd2926737f630458a36a89de8c59b9349bf2
09-hrick <sanyammahajan09@gmail.com>
2023-02-13 23:31:02 +0530

    made project named as banking system

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Header from './components/Header';
+import Home from './pages/Home'
+import MyProfile from './pages/MyProfile'
+import Transactions from './pages/Transactions'
+import NewUser from './pages/NewUser'
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import Register from './pages/Register';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPasssword';
+import SendMoney from './pages/SendMoney';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* {(Route.path == "/user/login" || Route.path == "/user/register") ? null : <Header />} */}
+      <Header/>
+
+      <Routes>
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/user/profile" element={<MyProfile />} />
+        <Route exact path="/user/transactions" element={<Transactions />} />
+        <Route exact path="/newuser" element={<NewUser />} />
+        <Route exact path="/user/register" element={<Register />} />
+        <Route exact path="/user/login" element={<Login />} />
+        <Route exact path="/user/forgotpassword" element={<ForgotPassword />} />
+        <Route exact path="/user/sendmoney" element={<SendMoney />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
